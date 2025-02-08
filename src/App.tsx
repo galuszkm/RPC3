a994f53f514ca3a7fa7513264cb3bf0d17b39e27
galuszkm <michal.galuszka1@gmail.com>
2025-02-08 14:56:51 +0100

    Added Splitter to App component between Chart and ChannelTable

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,48 @@ import 'primereact/resources/themes/lara-light-indigo/theme.css';
 import 'primeicons/primeicons.css';
 import { FilesProvider } from "./context/FilesContext";
 import { ChannelsProvider } from './context/ChannelsContext';
+import { Splitter, SplitterPanel } from 'primereact/splitter';
+import { Button } from 'primereact/button';
 import Dropzone from "./components/Dropzone";
 import Chart from "./components/Chart";
 import ChannelTable from './components/ChannelTable';
-import "./App.css"
+import "./App.css";
+
+const renderSplitter = () => (
+  <Splitter className='App-grid-split' style={{ height: '96vh', border: 'none' }} layout="vertical" gutterSize={6}>
+    <SplitterPanel size={50} minSize={20} style={{ overflow: 'hidden'}} >
+      <Chart />
+    </SplitterPanel>
+    <SplitterPanel size={50} minSize={20} style={{ overflow: 'auto', paddingTop: '0.5rem' }} >
+      <ChannelTable />
+  </SplitterPanel>
+  </Splitter>
+)
+
+const renderRightColumn = () => (
+  <div className='App-grid-utils-box'>
+    <div className='App-dropzone'>
+      <Dropzone />
+    </div>
+    <div className='App-utils'>
+      <Button 
+        className='cbutton'
+        style={{width: '100%'}}
+        label="Calculate damage and equivalent block signal" 
+        icon="pi pi-wrench" 
+        onClick={()=>{}}
+      />
+    </div>
+  </div>
+)
 
 export default function App() {
   return (
     <FilesProvider>
       <ChannelsProvider>
         <div className='App-grid'>
-          <div className='App-grid-chart'>
-            <Chart />
-            <ChannelTable />
-          </div>
-          <div className='App-grid-dropzone'>
-            <Dropzone />
-          </div>
+          {renderSplitter()}
+          {renderRightColumn()}
         </div>
       </ChannelsProvider>
     </FilesProvider>
